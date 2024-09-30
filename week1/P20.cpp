c81c7b8b9a4ae930d2f27d6dc95d4a8aff2166a6
박성현 <shpark8267@gmail.com>
2024-10-01 04:09:21 +0900

    Solve 프로그래머스 5일차 '배열 뒤집기'

---
+++ week1/P20.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> num_list)
+{
+	vector<int> answer;
+	for (int i = 0; i < num_list.size(); i++)
+		answer.push_back(num_list[num_list.size() - i - 1]);
+	return answer;
+}

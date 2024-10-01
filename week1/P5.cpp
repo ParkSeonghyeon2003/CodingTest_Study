3b9061a79c9028d4bc0cbff65fe2a3ab9d5ea907
박성현 <shpark8267@gmail.com>
2024-10-02 04:45:42 +0900

    Delete oldfiles

--- week1/P5.cpp
+++
@@ -1,6 +0,0 @@
-int solution(int num1, int num2) {
-    // double로 num1 캐스팅
-    // num1/num2*1000연산 수행 후 return 하면
-    // int로 암묵적 캐스팅되어 정수만 반환 됨.
-    return (double)num1 / num2 * 1000;
-}
--- week1/P6.cpp
+++
@@ -1,3 +0,0 @@
-int solution(int num1, int num2) {
-    return num1 == num2 ? 1 : -1; // 삼항연산자 사용
-}

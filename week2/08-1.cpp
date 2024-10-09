15ca148f99b4b6b84bd754331fc1042935587fa5
박성현 <shpark8267@gmail.com>
2024-10-09 22:37:13 +0900

    Refactoring 8-1

--- week2/08-1.cpp
+++ week2/08-1.cpp
@@ -3,11 +3,6 @@ using namespace std;
 
 vector<int> solution(vector<int> numbers, int num1, int num2)
 {
-	vector<int> answer;
-	vector<int>::iterator iter;
-	for (iter = numbers.begin() + num1;
-		iter != numbers.begin() + num2 + 1; ++iter) {
-		answer.push_back(*iter);
-	}
+	vector<int> answer(numbers.begin() + num1, numbers.begin() + num2 + 1);
 	return answer;
 }

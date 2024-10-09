ce1f90538ffcdfc1346a0e6423ed72811e3fcbf4
박성현 <shpark8267@gmail.com>
2024-10-09 21:35:10 +0900

    Refactoring 7-1

--- week2/07-1.cpp
+++ week2/07-1.cpp
@@ -1,11 +1,9 @@
 #include <string>
 using namespace std;
 
-string solution(string str, string letter)
-{
+string solution(string my_string, string letter) {
 	string answer = "";
-	char ltr = *letter.c_str();
-	for (auto& chr : str)
-		if (chr != ltr) answer += chr;
+	for (auto& chr : my_string) // iterator 순회. chr는 char&형이 됨.
+		if (chr != letter[0]) answer += chr; // letter가 아니면 answer에 추가
 	return answer;
 }

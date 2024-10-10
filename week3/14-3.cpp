afe65c4497c1e5aa1211fe10230e0509981b0a1e
박성현 <shpark8267@gmail.com>
2024-10-11 01:03:45 +0900

    Solve 14-3

---
+++ week3/14-3.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    int len = cipher.length();
+    for (int i = code - 1; i < len; i += code)
+        answer += cipher[i];
+    return answer;
+}

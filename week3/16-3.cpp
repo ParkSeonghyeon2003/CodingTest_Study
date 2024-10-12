db1e2b1a20562a1c474c1e2cba59636a02c55bf2
ParkSeonghyeon2003 <shpark8267@gmail.com>
2024-10-12 22:27:41 +0900

    Add comment 16-3 (문자열 파싱 및 POSTFIX 연산)

--- week3/16-3.cpp
+++ week3/16-3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 stack<int> numbers;
 
+// Push integer parsed from string.
 void parsing(const string& str)
 {
     string temp = "";
@@ -13,9 +14,11 @@ void parsing(const string& str)
     string::const_iterator iter;
     for (iter = str.begin(); iter < str.end(); ++iter)
     {
-        if (iter == str.end() - 1) temp += *iter;
+        if (iter == str.end() - 1) temp += *iter; // last char
+        // push integer when meet space or last char.
         if (*iter == ' ' || iter == str.end() - 1)
         {
+            // if latest operator = '-', multiply -1.
             pushElem = atoi(temp.c_str()) * sign;
             ++iter;
             if (*iter == '-') sign = -1;
